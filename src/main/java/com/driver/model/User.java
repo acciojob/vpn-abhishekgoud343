c08679592a7b64f3c0647de286bb7d8e923e91f2
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-12-07 20:26:57 +0530

    Final try

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -1,4 +1,126 @@
+//package com.driver.model;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "user")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//    private String username;
+//
+//    private String password;
+//
+//    private String originalIp;
+//
+//    private String maskedIp;
+//
+//    private boolean connected;
+//
+//    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+//    private Country originalCountry;
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<Connection> connectionList = new ArrayList<>();
+//
+//    @ManyToMany
+//    @JoinColumn
+//    List<ServiceProvider> serviceProviderList = new ArrayList<>();
+//
+//    public User(Integer id, String username, String password, String originalIp, String maskedIp, boolean connected, Country originalCountry, List<Connection> connectionList, List<ServiceProvider> serviceProviderList) {
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//        this.originalIp = originalIp;
+//        this.maskedIp = maskedIp;
+//        this.connected = connected;
+//        this.originalCountry = originalCountry;
+//        this.connectionList = connectionList;
+//        this.serviceProviderList = serviceProviderList;
+//    }
+//
+//    public User() {
+//    }
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getOriginalIp() {
+//        return originalIp;
+//    }
+//
+//    public void setOriginalIp(String originalIp) {
+//        this.originalIp = originalIp;
+//    }
+//
+//    public String getMaskedIp() {
+//        return maskedIp;
+//    }
+//
+//    public void setMaskedIp(String maskedIp) {
+//        this.maskedIp = maskedIp;
+//    }
+//
+//    public boolean getConnected() {
+//        return connected;
+//    }
+//
+//    public void setConnected(boolean connected) {
+//        this.connected = connected;
+//    }
+//
+//    public Country getOriginalCountry() {
+//        return originalCountry;
+//    }
+//
+//    public void setOriginalCountry(Country originalCountry) {
+//        this.originalCountry = originalCountry;
+//    }
+//
+//    public List<Connection> getConnectionList() {
+//        return connectionList;
+//    }
+//
+//    public void setConnectionList(List<Connection> connectionList) {
+//        this.connectionList = connectionList;
+//    }
+//
+//    public List<ServiceProvider> getServiceProviderList() {
+//        return serviceProviderList;
+//    }
+//
+//    public void setServiceProviderList(List<ServiceProvider> serviceProviderList) {
+//        this.serviceProviderList = serviceProviderList;
+//    }
+//}
+
 package com.driver.model;
+
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -9,29 +131,28 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     private String username;
-
     private String password;
-
     private String originalIp;
-
     private String maskedIp;
+    private Boolean connected;
 
-    private boolean connected;
-
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
     private Country originalCountry;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Connection> connectionList = new ArrayList<>();
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Connection> connectionList=new ArrayList<>();
 
-    @ManyToMany
     @JoinColumn
-    List<ServiceProvider> serviceProviderList = new ArrayList<>();
+    @ManyToMany
+    private List<ServiceProvider> serviceProviderList=new ArrayList<>();
 
-    public User(Integer id, String username, String password, String originalIp, String maskedIp, boolean connected, Country originalCountry, List<Connection> connectionList, List<ServiceProvider> serviceProviderList) {
+    public User() {
+    }
+
+    public User(int id, String username, String password, String originalIp, String maskedIp, Boolean connected, Country originalCountry, List<Connection> connectionList, List<ServiceProvider> serviceProviderList) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -43,14 +164,11 @@ public class User {
         this.serviceProviderList = serviceProviderList;
     }
 
-    public User() {
-    }
-
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -86,11 +204,11 @@ public class User {
         this.maskedIp = maskedIp;
     }
 
-    public boolean getConnected() {
+    public Boolean getConnected() {
         return connected;
     }
 
-    public void setConnected(boolean connected) {
+    public void setConnected(Boolean connected) {
         this.connected = connected;
     }
 
